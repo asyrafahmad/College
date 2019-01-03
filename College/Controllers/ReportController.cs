2fe895227683547c8679e75c5bfe978c7d1b1c3e
Asyraf Ahmad <are.syraf9764@gmail.com>
2019-01-04 03:14:00 +0800

    Commit Initial

---
+++ College/Controllers/ReportController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace College.Controllers
+{
+    public class ReportController : Controller
+    {
+        // GET: Report
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ College/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(College.Startup))]
+namespace College
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
