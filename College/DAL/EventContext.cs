f460d77943a049d6fabef29878d9464daea562dd
iman apek <imanapekyo@gmail.com>
2019-01-04 15:43:57 +0800

    CRUD for event

---
+++ College/DAL/EventContext.cs
@@ -0,0 +1,19 @@
+﻿using College.Models;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace College.DAL
+{
+    public class EventContext : DbContext
+    {
+        public EventContext() : base("EventContext ")
+        {
+        }
+        public DbSet<Event> Events { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+           protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        }
+    }
+}
