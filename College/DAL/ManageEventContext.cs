9c3639ca8388870fbf2baa27cd10fae31c923a22
iman apek <imanapekyo@gmail.com>
2019-01-04 18:02:07 +0800

    try crud event

---
+++ College/DAL/ManageEventContext.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Data.Entity;
+using College.Models;
+
+namespace College.DAL
+{
+    public class ManageEventContext : DbContext
+    {
+
+        public ManageEventContext() : base("ManageEventContext")
+        {
+        }
+
+        public DbSet<Event> Events { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ College/Models/Event.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace College.Models
+{
+    public class Student
+    {
+        public int EventID { get; set; }
+        public string EventName { get; set; }
+        public DateTime EventDate { get; set; }
+        public string EventPlace { get; set; }
+        public string EventDescription { get; set; }
+       
+    }
+}
