f8a31e32924ab1c1f3db013d95adea709a3356da
User <User@DESKTOP-Q2AMJ2Q>
2019-01-05 15:22:58 +0800

    ssss

--- College/Global.asax.cs
+++ College/Global.asax.cs
@@ -10,6 +10,7 @@ namespace College
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
